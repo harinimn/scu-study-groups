4a9072f7067ac5ac3b695979a1ca9601490e4963
Itay Volk <itay.volk1@gmail.com>
2026-01-21 14:25:40 -0800

    Split into multiple files

---
+++ functions/profile.mjs
@@ -0,0 +1,41 @@
+// Handles methods related to seting up a profile
+
+import { onCall, HttpsError } from "firebase-functions/v2/https";
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+initializeApp();
+const db = getFirestore();
+
+export const setup = onCall(async (data, context) => {
+  const uid = context.auth.uid;
+  if (!uid) {
+    throw new HttpsError('unauthenticated', 'User must be authenticated to call this function.');
+  }
+
+  const def = data.def_vis;
+  for (const [key, value] of data) {
+    if (key.endsWith("_vis") && key != "def_vis" && value < def) {
+      data.set(key, def);
+    }
+  }
+
+  const res = await db.doc(uid).set(data);
+  return res;
+});
+
+export const get = onCall(async (data, context) => {
+  const uid = context.auth.uid;
+  if (!uid) {
+    throw new HttpsError('unauthenticated', 'User must be authenticated to call this function.');
+  }
+
+  const res = await db.doc(uid).get();
+  if (!res.exists) {
+    throw new HttpsError('not-found', 'This user  doesn\'t have any data.');
+  }
+
+  let out = res.data();
+  await out.delete("courses");
+  return out;
+});
